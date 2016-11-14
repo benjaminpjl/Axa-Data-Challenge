b494548af230fa0cb76ac5322713d51f58b4dcf2
benjaminpjl <benjaminpujol@hotmail.fr>
2016-11-14 08:41:13 +0100

    Update model.py

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-import sys
+mport sys
 import os
 sys.path.append(os.path.abspath("/Users/benjaminpujol/Desktop/Projet-avec-le-mexicain"))
 import features_preprocessing as fp
@@ -13,45 +13,31 @@ import pandas as pd
 from sklearn import linear_model
 
 preprocessing = fp.feature_preprocessing()
-preprocessing.full_preprocess(used_columns=['ASS_ID', 'TIME', 'WEEK_DAY', 'CSPL_RECEIVED_CALLS'])
+preprocessing.full_preprocess(used_columns=['ASS_ID', 'TIME', 'WEEK_DAY', 'CSPL_RECEIVED_CALLS'])  #Choose the columns you want to use, always keep at least ASS_ID, TIME, WEEK_DAY AND CSPL_RECEIVED_CALLS
 data = preprocessing.data
+
 Y = data['CSPL_RECEIVED_CALLS']
 X = data.drop(['CSPL_RECEIVED_CALLS'], axis=1)
-print(np.unique(X['WEEK_DAY'].values))
 
 
-
-Y = data['CSPL_RECEIVED_CALLS']
-X= data.drop(['CSPL_RECEIVED_CALLS'], axis=1)
-X_train, X_cv, Y_train, Y_cv = cross_validation.train_test_split(X, Y, test_size=0.4, random_state=0)
+#Implement cross validation (10 splits)
+cv_score = 0
+for size in [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]:
+    X_train, X_cv, Y_train, Y_cv = cross_validation.train_test_split(X, Y, test_size = size, random_state=0)
 
 #RandomForest
 
+    clf = RandomForestRegressor(n_estimators=1000, oob_score=True)
+    clf.fit(X_train,Y_train)
+    cv_score += clf.score(X_cv,Y_cv)
+print('cv_score', cv_score/10) # Print average scores
 
-clf = RandomForestRegressor(n_estimators=1000, oob_score=True)
-clf.fit(X_train,Y_train)
-print(clf.score(X_cv,Y_cv))
-
-
-submission = sp.submission_preprocessing()
-submission.full_preprocess(used_columns=['ASS_ID', 'TIME', 'WEEK_DAY', 'CSPL_RECEIVED_CALLS'])
+submission = sp.submission_preprocessing()  #Preprocess submission.txt
+submission.full_preprocess(used_columns=['ASS_ID', 'TIME', 'WEEK_DAY', 'CSPL_RECEIVED_CALLS']) #Choose columns
 sub_data = submission.data
 Y_test = data['CSPL_RECEIVED_CALLS']
 X_test = data.drop(['CSPL_RECEIVED_CALLS'], axis=1)
 
-Y_pred = clf.predict(X_test)
-print(ln.evalerror_linex(Y_pred,Y_test))
+Y_pred = clf.predict(X_test)            #Predict Y_pred from X_test
+print(ln.evalerror_linex(Y_pred,Y_test))  #Print Linex error
 
-
-
-
-
-
-
-
-#AdaboostRegression
-
-#clf2 = AdaBoostRegressor(n_estimators=100)
-#clf2.fit(X_train,Y_train)
-#predict_2 = clf2.predict(X_test)
-#print(clf2.score(X_test,Y_test))
