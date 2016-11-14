512da9d11c39030cac3c724a3941aff6f037268a
benjaminpjl <benjaminpujol@hotmail.fr>
2016-11-14 08:41:47 +0100

    Update load_data

--- load_data.py
+++ load_data.py
@@ -1,12 +1,12 @@
 import sys
 import os
-sys.path.append(os.path.abspath("/Users/benjaminpujol/Desktop/DataChallenge/Code/"))
+sys.path.append(os.path.abspath("/Users/benjaminpujol/Desktop/Projet-avec-le-mexicain/Code/"))
 import pandas as pd
 import time
 from configuration import CONFIG
 
 
-def find_day(day):
+def find_day(day):             #Transforme les jours de la semaine en int compris entre 0 et 1
     if (day == "Dimanche"):
         return 0
     if (day == "Lundi"):
@@ -23,7 +23,7 @@ def find_day(day):
         return 6
 
 
-def get_year_day(date):
+def get_year_day(date):                                             #Retourne le jour de l'année
     parsedTime = time.strptime(date, "%Y-%m-%d %H:%M:%S.000")
     return parsedTime.tm_yday, parsedTime.tm_year
 
@@ -34,14 +34,14 @@ def normalize(df, column):
     var = (((df[column] - moyenne) ** 2).mean()) ** 0.5
     df[column] = df[column].apply(lambda pre: (pre - moyenne) / (var))
 
-
+#Loading data
 class load_data:
     def __init__(self):
-        data = pd.read_csv('train_2011_2012_2013.csv', sep=";", usecols=CONFIG.useful_columns)
-        week_day = data['DAY_WE_DS'].map(lambda day: find_day(day))
+        data = pd.read_csv('train_2011_2012_2013.csv', sep=";", usecols=CONFIG.useful_columns, nrows = 1000000)  #Choose nrows, number of rows
+        week_day = data['DAY_WE_DS'].map(lambda day: find_day(day))  #Dimanche->0, Lundi->1, Mardi->2, etc...
         data['WEEK_DAY'] = week_day
-        data['ASS_ID'] = data['ASS_ASSIGNMENT'].apply(lambda x: int(CONFIG.ass_assign[x]))
-        data = data.drop(['ASS_ASSIGNMENT', 'DAY_WE_DS', 'WEEK_END'], axis=1)
+        data['ASS_ID'] = data['ASS_ASSIGNMENT'].apply(lambda x: int(CONFIG.ass_assign[x])) #Create ASS_ID (int between 0 and 28) from ASS_ASSIGNMENT as defined in configuration.py
+        data = data.drop(['ASS_ASSIGNMENT', 'DAY_WE_DS', 'WEEK_END'], axis=1) #Drop 3 columns
         self.data = data
         #self.data = data.groupby(['DATE', 'ASS_ID', 'DAY_OFF', 'WEEK_DAY'], sort=False).sum().reset_index()
 
@@ -49,7 +49,7 @@ class load_data:
 
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  #Execute uniquement si le fichier load_data est executé directement et pas importé
     loader = load_data()
     loader.data.to_csv('../preprocessed_data.csv', sep=";")
     loaded_data = loader.data
