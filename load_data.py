e5507f2998e6893cde4dc20b88d9732ab5f83aec
benjaminpjl <benjaminpujol@hotmail.fr>
2016-11-21 14:32:50 +0100

    Add files via upload

--- load_data.py
+++ load_data.py
@@ -6,22 +6,6 @@ import time
 from configuration import CONFIG
 
 
-def find_day(day):             #Transforme les jours de la semaine en int compris entre 0 et 1
-    if (day == "Dimanche"):
-        return 0
-    if (day == "Lundi"):
-        return 1
-    if (day == "Mardi"):
-        return 2
-    if (day == "Mercredi"):
-        return 3
-    if (day == "Jeudi"):
-        return 4
-    if (day == "Vendredi"):
-        return 5
-    if (day == "Samedi"):
-        return 6
-
 
 def get_year_day(date):                                             #Retourne le jour de l'année
     parsedTime = time.strptime(date, "%Y-%m-%d %H:%M:%S.000")
@@ -37,32 +21,14 @@ def normalize(df, column):
 #Loading data
 class load_data:
     def __init__(self):
-        data = pd.read_csv('train_2011_2012_2013.csv', sep=";", usecols=CONFIG.useful_columns, nrows = 300000)  #Choose nrows, number of rows
-        data.set_index('DATE')
-        nrows = len(data.index)
-        
-        #Creating features Jour et Nuit
-        tper_team = data['TPER_TEAM'].values.tolist()
-    
-        jour = []
-        nuit = []
-    
-        for i in range(nrows):
-            if(tper_team[i] == "Jours"):
-                jour.append(1)
-                nuit.append(0)
-            else:
-                nuit.append(1)
-                jour.append(0)
-
-        data['JOUR'] = jour
-        data['NUIT'] = nuit
+        data = pd.read_csv('train_2011_2012_2013.csv', sep=";", usecols=CONFIG.useful_columns, nrows = 300000)
         
         week_day = data['DAY_WE_DS'].map(lambda day: find_day(day))  #Dimanche->0, Lundi->1, Mardi->2, etc...
         data['WEEK_DAY'] = week_day
-        data['ASS_ID'] = data['ASS_ASSIGNMENT'].apply(lambda x: int(CONFIG.ass_assign[x])) #Create ASS_ID (int between 0 and 28) from ASS_ASSIGNMENT as defined in configuration.py
-        data = data.drop(['ASS_ASSIGNMENT', 'DAY_WE_DS', 'WEEK_END'], axis=1) #Drop 3 columns
+        
+        data = data.drop(['DAY_WE_DS', 'WEEK_END'], axis=1) #Drop 3 columns
         self.data = data
+        print(self.data)
         #self.data = data.groupby(['DATE', 'ASS_ID', 'DAY_OFF', 'WEEK_DAY'], sort=False).sum().reset_index()
 
 
