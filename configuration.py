333242fc564cf71bce85d2b9700862cbf6a9287a
benjaminpjl <benjaminpujol@hotmail.fr>
2016-11-23 13:08:03 +0100

    Changes BP

--- configuration.py
+++ configuration.py
@@ -6,7 +6,7 @@ class _Config:
         self.days = {1: "MONDAY", 2: "TUESDAY", 3: "WEDNESDAY", 4: "THURSDAY", 5: 'FRIDAY', 6: "SATURDAY", 0: "SUNDAY"}
         self.ass_assign = {'Téléphonie':0, 'RTC':1, 'Gestion Renault':2, 'Nuit':3,'Gestion - Accueil Telephonique':4, 'Regulation Medicale':5, 'Services':6,'Tech. Total':7, 'Gestion Relation Clienteles':8, 'Crises':9, 'Japon':10, 'Médical':11, 'Gestion Assurances':12, 'Domicile':13, 'Gestion':14, 'SAP':15, 'RENAULT':16, 'Gestion Amex':18, 'Tech. Inter':19, 'Gestion Clients':20, 'Manager':21, 'Tech. Axa':22, 'CAT':23, 'Gestion DZ':24, 'Mécanicien':25, 'CMS':26, 'Prestataires':27, 'Evenements':28}
 
-        self.default_columns = list(self.days.values())+ ['TIME', 'JOUR','NUIT','DAY_OFF','ASS_ID']+ ['CSPL_RECEIVED_CALLS']
+        self.default_columns = list(self.days.values())+ ['TIME', 'JOUR','NUIT','DAY_OFF']+ ['CSPL_RECEIVED_CALLS']
 CONFIG = _Config()
 
 print (CONFIG.default_columns)
