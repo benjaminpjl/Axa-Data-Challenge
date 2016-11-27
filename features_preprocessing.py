145f4138f3fdcc558ffc146ef7539adb1d9b2fde
benjaminpjl <benjaminpujol@hotmail.fr>
2016-11-27 12:46:04 +0100

    Update features_preprocessing.py

--- features_preprocessing.py
+++ features_preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
-import time
 from configuration import CONFIG
+import numpy as np
+import datetime as dt
 
 
 def find_day(day):             #Transforme les jours de la semaine en int compris entre 0 et 1
@@ -19,44 +20,55 @@ def find_day(day):             #Transforme les jours de la semaine en int compri
     if (day == "Samedi"):
         return 6
 
-def get_year_day(date):                                             #Retourne le jour de l'année
-    parsedTime = time.strptime(date, "%Y-%m-%d %H:%M:%S.000")
-    return parsedTime.tm_yday, parsedTime.tm_year
 
+def extract_date(string):
+    d = dt.datetime.strptime(string, "%Y-%m-%d %H:%M:%S.000")
+    return(d)
 
-def date_reducer(date):
-    parsedTime =  time.strptime(date, "%Y-%m-%d %H:%M:%S.000")
-    year = int(parsedTime.tm_year)
-    month = int(parsedTime.tm_mon)
-    day = int(parsedTime.tm_mday)
-    hour = int(parsedTime.tm_hour*60 + parsedTime.tm_min)
-    year_day = int(parsedTime.tm_yday)
+def extract_weekday(date):
+    return(date.weekday())
 
-    return hour, year_day, day, month, year
+def extract_hour(date):
+    return(date.hour)
+
+def extract_month(date):
+    return(date.month)
+
+def extract_year(date):
+    return(date.year)
 
 class feature_preprocessing():
 
-    def __init__(self):
-        self.data = pd.read_csv('train_2011_2012_2013.csv', sep=";", usecols=CONFIG.useful_columns, nrows =5000000)
-
+    def __init__(self, filename = None, sepa = None, usecols = CONFIG.useful_columns):
+        if filename == None and sepa == None:
+            self.data = pd.DataFrame()
+        else:
+            self.data = pd.read_csv(filename, sep=sepa , usecols=usecols, nrows = 500000)
+    
 
     def preprocess_date(self):
-        self.data["DATE"] = self.data ["DATE"].apply(date_reducer)
+        self.data["DATE"] = self.data ["DATE"].apply(extract_date)
         #self.data = self.data.groupby(['DATE','ASS_ID','DAY_OFF','WEEK_DAY']).sum()
         #self.data = self.data.reset_index()
 
     def select_assid(self, assid):
         self.data=self.data.loc[self.data['ASS_ID'] == assid]
     
+    
 
     def date_vector(self):
-        self.data['YEAR'] = self.data['DATE'].apply(lambda x: x[4])
-        self.data['MONTH'] = self.data['DATE'].apply(lambda x: x[3])
+        self.data['YEAR'] = self.data['DATE'].apply(extract_year)
+        for year in ['2011','2012','2013']:
+            self.data[year] = self.data['YEAR'].apply(lambda x: (int(year) == x)*1)
 
+        self.data['MONTH'] = self.data['DATE'].apply(extract_month)
         for key, month in CONFIG.months.items():
             self.data[month] = self.data['MONTH'].apply(lambda x: int(x == key))
-        self.data['TIME'] = self.data ["DATE"].apply(lambda x: x[0])
-        self.data['YEAR_DAY']= self.data["DATE"].apply(lambda x: x[1])
+        
+        self.data['TIME']= self.data['DATE'].apply(extract_hour)
+        #self.data['TIME'] = self.data['TIME'].apply(lambda x: x in range(450,1411)*(x[0]-450)/30 + x in range(0,451)*(x/30+1) + x in range(1411,1441)*0)
+        self.data['YEAR_DAY']= self.data["DATE"].apply(extract_weekday)
+        
 
     def ass_id_creation(self): # Create ASS_ID (int between 0 and 28) from ASS_ASSIGNMENT as defined in configuration.py
         self.data['ASS_ID'] = self.data['ASS_ASSIGNMENT'].apply(lambda x: int(CONFIG.ass_assign[x]))
@@ -65,6 +77,32 @@ class feature_preprocessing():
         its = self.data['ASS_ASSIGNMENT'].unique()
         for it in its:
             self.data[it]= self.data['ASS_ASSIGNMENT'].apply(lambda x: int(x==it))
+
+    def hourlymean_past(date, y):
+    
+        nday_before = 7
+        ysel = y.loc[(y.index > date - dt.timedelta(nday_before))]
+        ysel = ysel.loc[(ysel.index < date)]
+        h = extract_hour(date)
+        if (len(ysel.loc[ysel.loc[:,'HOUR'] == h]) < 0):
+            return(0)
+        else:
+            return(ysel.loc[ysel.loc[:,'HOUR'] == h].loc[:,"CSPL_RECEIVED_CALLS"].mean())
+
+
+    def lastvalue(date, y):
+        nday_before = 7
+        try:
+            v = y.loc[date - dt.timedelta(nday_before)]["CSPL_RECEIVED_CALLS"]
+            try:
+                w = y.loc[date - 2*dt.timedelta(nday_before)]["CSPL_RECEIVED_CALLS"]
+            except:
+                w = v
+        except KeyError:
+            v = y.loc[date]["CSPL_RECEIVED_CALLS"] + np.random.normal(scale = 3)
+            w = v
+        return((3*v + w)/4)
+
 
     def jour_nuit_creation(self):  #Création de la feature jour nuit
 
@@ -97,11 +135,14 @@ class feature_preprocessing():
         if keep_all_ass_id!=True:
             self.select_assid(assid)
         self.preprocess_date()
+        self.date_vector()
+        self.hourlymean_past(self.data['DATE'],self.data)
         self.jour_nuit_creation()
         self.week_day_to_vector()
         #self.ass_assignement_to_vector()
-        self.date_vector()
+        print(self.data.head(100))
         self.data = self.data[CONFIG.default_columns]
+        
  
         
 
